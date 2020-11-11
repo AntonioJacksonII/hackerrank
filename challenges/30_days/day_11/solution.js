d5dd9bce99d9753df4e6ae859756530481f040eb
Antonio Jackson <antoniojackson@gmail.com>
2020-11-11 16:33:49 -0700

    Adding Day 11 test case and solution

---
+++ challenges/30_days/day_11/solution.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+    
+    let hourglassSums = [];
+    
+    for (let i = 1; i < 5; i++) {
+        for (let j = 1; j < 5; j++) {
+            let hourglassSum = arr[i][j] + arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1];
+            hourglassSums.push(hourglassSum);
+        }
+    }
+    console.log(Math.max(...hourglassSums));
+}
