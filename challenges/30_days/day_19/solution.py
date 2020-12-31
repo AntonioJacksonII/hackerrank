01895f62048618fe787aacd60d78f0106b180d46
Antonio Jackson <antoniojackson@gmail.com>
2020-12-30 23:23:26 -0700

    Adding Day 19: Interfaces Python test using pytest

--- challenges/30_days/day_19/solution.py
+++ challenges/30_days/day_19/solution.py
@@ -13,8 +13,8 @@ class Calculator(AdvancedArithmetic):
         return total
 
 
-n = int(input())
-my_calculator = Calculator()
-s = my_calculator.divisorSum(n)
-print("I implemented: " + type(my_calculator).__bases__[0].__name__)
-print(s)
+# n = int(input())
+# my_calculator = Calculator()
+# s = my_calculator.divisorSum(n)
+# print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+# print(s)
---
+++ challenges/30_days/day_19/test_solution.py
@@ -0,0 +1,5 @@
+from solution import *
+
+def test_solution():
+    test_calculator = Calculator()
+    assert test_calculator.divisorSum(6) == 12
